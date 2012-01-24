bb55ad768efc5c260b8cf35b1be5cd7452ff83ac
Bruno Baia <brbaia@gmail.com>
2012-01-24 21:37:51 +0100

    Initialized project skeleton

---
+++ src/Spring.Social.LinkedIn/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Spring.Social.LinkedIn")]
+[assembly: AssemblyDescription("Spring.NET Social Framework extension with connection support and an API binding for LinkedIn")]
+
+
---
+++ test/Spring.Social.LinkedIn.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Spring.Social.LinkedIn Tests")]
+[assembly: AssemblyDescription("Unit tests for the Spring.Social.LinkedIn assembly")]
